df770a35832b097ef5b0081120592506bf8dbf1e
Divya <divyadivyashankar011@gmail.com>
2026-09-04 20:59:11 +0530

    updated full code

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -14,7 +14,7 @@ import 'app_state.dart';
 /// Example: 'https://sumathistyles.up.railway.app'
 /// Do NOT add a trailing slash.
 /// -----------------------------------------------------------------
-const String baseUrl = 'http://localhost/fashion/backend';
+const String baseUrl = 'http://192.168.1.5/fashion/backend';
 
 /// A tailoring/stitching service offered by the shop.
 class StitchingService {
