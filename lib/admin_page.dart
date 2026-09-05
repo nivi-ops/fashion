f80595aa9abedca45bc27f11a81f55a1f4fa545f
Divya <divyadivyashankar011@gmail.com>
2026-09-05 15:34:01 +0530

    updated full code

--- lib/admin_page.dart
+++ lib/admin_page.dart
@@ -29,7 +29,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class AdminPage extends StatefulWidget {
   const AdminPage({
     super.key,
-    this.baseUrl = 'https://fashion-production-9a4b.up.railway.app/admin.html',
+    this.baseUrl = 'https://fashion-production-9a4b.up.railway.app',
   });
 
   /// Example:
